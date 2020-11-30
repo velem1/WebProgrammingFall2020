8bbf58f435fbf93eee564459456182401b2a41c4
Melissa Vele <melissavele1@gmail.com>
2020-11-29 19:38:05 -0500

    oAuth Facebook and Google

--- client/src/views/Feed.vue
+++ client/src/views/Feed.vue
@@ -3,13 +3,32 @@
       <h2 class="title is-2">Feed Page</h2>
 
         <div class="columns">
-            <div class="column is-two-thirds">
+            <div class="column is-one-quarter" @mouseenter="error">
+                <Sidebar />
+            </div>
+            <div class="column is-one-half">
+
+                <div class="card">
+                    <header class="card-header">
+                        <p class="card-header-title">
+                            Add a picture
+                        </p>
+                    </header>
+                   <!-- <header>
+                      <p class="card-content">
+                        <figure v-for="p in fbPics" :key="p.id" class="image is-64x64" @click.prevent="add(p)">
+                            <img :src="p.picture" alt="" />
+                        </figure>
+                      </p>
+                    </header> -->
+                </div>
+
                 <Post v-for=" (x, i) in posts " 
                       :key="i"
                       :i="i"
                       :post="x" /> 
             </div>
-            <div class="column is-one-third" @mouseenter="error">
+            <div class="column is-one-quarter" @mouseenter="error">
                 <Sidebar />
             </div>
         </div>
@@ -19,20 +38,33 @@
 <script>
 import Sidebar from "@/components/Sidebar";
 import Post from "@/components/Post";
-import { posts } from "@/models/feed";
+import { getPosts } from "@/models/feed";
 import session from "@/models/session";
 export default {
     data(){
         return {
-            posts
+            posts: [],
+            fbPics: []
         }
+    },
+    async created(){
+        this.posts = await getPosts();
+        FB.api("me/photos?fields=link,images,picture", x=>{
+            this.fbPics = x.data
+            console.log(x)
+        })
     },
     components: {
         Sidebar, Post
     },
     methods: {
         error(){
-            session.addNotification('Something went wrong.', 'danger')
+            //session.addNotification('Something went wrong.', 'danger')
+        },
+        add(p){
+            this.posts.push({
+                URL: p.images[0].source
+            })
         }
     }
 }
